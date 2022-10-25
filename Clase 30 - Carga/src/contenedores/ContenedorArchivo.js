4b7eb4825611b8b5b244a554626c989fae045394
bswalter8 <bswalter8@gmail.com>
2022-10-25 02:16:55 -0300

    Entrega carga

---
+++ Clase 30 - Carga/src/contenedores/ContenedorArchivo.js
@@ -0,0 +1,103 @@
+import { promises } from "fs";
+const { readFile, writeFile } = promises;
+
+class ContenedorArchivo {
+  
+    constructor(archivo) {
+      this.archivoNombre = archivo;
+    //  this.id;
+    }
+  
+    async save(elem) {
+      const elems = await this.getAll()
+
+      let newId
+      if (elems.length == 0) {
+          newId = 1
+      } else {
+          newId = elems[elems.length - 1].id + 1
+      }
+
+      const newElem = { ...elem, id: newId }
+      elems.push(newElem)
+
+      try {
+          await writeFile(this.archivoNombre, JSON.stringify(elems),"utf-8");
+          
+          return newId
+      } catch (error) {
+          throw new Error(`Error al guardar: ${error}`)
+      }
+    }
+  
+    async getById(id_Requerida) {
+      try {
+        let id = null;
+        let contenidoLeido = [];
+        let contenido = await readFile(this.archivoNombre, "utf-8");
+        contenidoLeido = JSON.parse(contenido);
+  
+        contenidoLeido.forEach((element) => {
+          if (element.id == id_Requerida) {
+            id = element;
+          }
+        });
+        return id;
+      } catch (error) {
+        console.log(`OHHHHH nooooooo!!!! ${error}`);
+      }
+    }
+  
+    async deleteById(id_Requerida) {
+      try {
+        let contenidoLeido = [];
+  
+        let contenido = await readFile(this.archivoNombre, "utf-8");
+        contenidoLeido = JSON.parse(contenido);
+  
+        contenidoLeido.forEach((element, i) => {
+          if (element.id == id_Requerida) {
+            contenidoLeido.splice(i, 1);
+          }
+        });
+  
+        try {
+          await writeFile(
+            this.archivoNombre,
+            JSON.stringify(contenidoLeido),
+            "utf-8"
+          );
+          console.log(`Se ha borrado el elemento con ID: ${id_Requerida}`);
+        } catch (error) {
+          console.log(`OHHHHH nooooooo!!!! borrado ${error}`);
+        }
+      } catch (error) {
+        console.log(`OHHHHH nooooooo!!!! ${error}`);
+      }
+    }
+  
+    async getAll() {
+      try {
+        let id;
+        let contenidoLeido = [];
+        let contenido = await readFile(this.archivoNombre, "utf-8");
+        if (contenido != ''){
+            contenidoLeido = JSON.parse(contenido);}
+      
+        return contenidoLeido;
+      } catch (error) {
+        console.log(`OHHHHH nooooooo!!!! no puede leer ${error}, ${this.archivoNombre}`);
+      }
+    }
+  
+    async deleteAll() {
+      try {
+        await writeFile(this.archivoNombre, "", "utf-8");
+        console.log(`Se ha borrado todo el contenido`);
+      } catch (error) {
+        console.log(`OHHHHH nooooooo!!!! borrado ${error}`);
+      }
+    }
+  }
+
+export default ContenedorArchivo
---
+++ Clase 30 - Carga/src/randoms.js
@@ -0,0 +1,43 @@
+
+
+
+  process.on('message', (msg) => {
+
+    if (msg) {
+        console.log('Child process received START message');
+  
+   
+    try{
+        const obj = {};
+       const min = 1;
+         const max = 1000;
+         let cantidadCal = 0;
+         msg.mensaje !== 0 ?   cantidadCal = msg.mensaje :  cantidadCal = 1e8;
+        
+         for(let i=0; i < cantidadCal; i++){
+            const random = Math.floor((Math.random() * (max - min + 1)) + min);
+            if(obj[random]){
+                obj[random] += 1
+            }else{
+                obj[random] = 1
+            }
+        }
+        console.log(obj)
+        process.send(obj);
+
+   
+     }catch(error){
+         console.log(`error salida del fork hijo: ${error}`);
+     }
+
+    }
+ });
+
+
+
+
+
+
+
+
+
