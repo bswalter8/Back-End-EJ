8da494197f3956f8ea69d682bfd0bb6e408135be
bswalter8 <bswalter8@gmail.com>
2022-11-01 23:40:02 -0300

    log

---
+++ Clase 32 - Logs/src/options/mysqlDB.js
@@ -0,0 +1,9 @@
+export const config = {
+    client : 'mysql',
+    connection: {
+        host: '192.168.64.2',
+        user:'root',
+        password:'',
+        database:'Ej16'
+    }
+}
---
+++ Clase 32 - Logs/src/routes.js
@@ -0,0 +1,135 @@
+import path from 'path';
+import { fork } from 'child_process';
+import { cpus } from "os";
+import { stringify } from 'querystring';
+
+const forked = fork('./src/randoms.js');
+
+function getRoot(req, res) {
+    res.send('Bienvenido');
+}
+
+const redirect = (req, res) => {
+    res.redirect('/login')
+ }
+
+function getLogin(req, res) {
+    //if (req.isAuthenticated()) {
+    if (req.isAuthenticated) {  
+        let user = req.user;
+       res.render('login-ok', {
+            usuario: user.username,
+            nombre: user.firstName,
+            apellido: user.lastName,
+            email: user.email
+        });
+ 
+      //  res.sendFile(path.join(process.cwd(), '../public/productos.html'));
+    }
+    else {
+       res.sendFile(path.join(process.cwd(), './public/login.html'));
+     
+    }
+}
+
+function getSignUp(req, res) {
+    res.sendFile(path.join(process.cwd(), './public/signup.html'));
+}
+
+
+function postLogin(req, res) {
+    const user = req.user;
+    console.log(user);
+    res.sendFile(path.join(process.cwd(), './public/index.html'));
+}
+
+function postSignup(req, res) {
+    const user = req.user;
+    console.log(user);
+    res.sendFile(path.join(process.cwd(), './public/index.html'));
+}
+
+function getFailLogin(req, res) {
+    console.log('error en login');
+    res.sendFile(path.join(process.cwd(), './public/login-fail.html'));
+    res.render('login-error', {
+    });
+ 
+}
+
+function getFailsignup(req, res) {
+    console.log('error en signup');
+    res.render('signup-error', {
+    });
+}
+
+
+function getLogout(req, res) {
+    req.logout((err) => {
+        if (err) { return next(err); }
+        res.sendFile(path.join(process.cwd(), './public/index.html'));
+    });
+}
+
+const getInfoWithConsoleLog = async (req, res) => {
+
+    const respuesta = {
+        Argumentos_de_entrada: process.argv,
+        Nombre_de_la_plataforma: process.platform,
+        Versión_de_node: process.version,                                               
+        Memoria_total_reservada: process.memoryUsage(),
+        Carpeta_del_proyecto: process.cwd(),
+        Process_id: process.pid,
+        Path_de_ejecución: process.execPath,
+        Procesadores: cpus().length
+    }
+    res.json(respuesta)
+    console.log(JSON.stringify(respuesta))
+}
+
+const getInfo = async (req, res) => {
+
+    const respuesta = {
+        Argumentos_de_entrada: process.argv,
+        Nombre_de_la_plataforma: process.platform,
+        Versión_de_node: process.version,                                               
+        Memoria_total_reservada: process.memoryUsage(),
+        Carpeta_del_proyecto: process.cwd(),
+        Process_id: process.pid,
+        Path_de_ejecución: process.execPath,
+        Procesadores: cpus().length
+    }
+    res.json(respuesta)
+}
+
+
+
+
+const getCalc = (req, res) => {
+    const cant = req.query.cant ? Number(req.query.cant) : 0;
+/*        console.log(cant)
+      forked.send({mensaje: cant})  
+      forked.on('message', msg => {
+        const num = JSON.stringify(msg)
+        res.end(num);
+    })*/
+    res.end(cant);
+}
+
+
+
+
+export {
+    getRoot,
+    redirect,
+    getLogin,
+    postLogin,
+    getFailLogin,
+    getLogout,
+    getSignUp,
+    postSignup,
+    getFailsignup,
+    getInfo,
+    getInfoWithConsoleLog,
+    getCalc
+}
