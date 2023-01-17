07de3f3a70153d10ca5967861bed6bd8ea020bf6
bswalter8 <bswalter8@gmail.com>
2023-01-17 04:20:48 -0300

    deno

---
+++ Clase 48 - Deno/deps.ts
@@ -0,0 +1,3 @@
+//export { Application } from "https://deno.land/x/oak/mod.ts";
+export { Application, Router, Context, helpers } from "https://deno.land/x/oak@v10.6.0/mod.ts"
+export { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts"
